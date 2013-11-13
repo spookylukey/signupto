198f42a0c2bddddea4c7317f915dec3f1d63192a
Luke Plant <L.Plant.98@cantab.net>
2013-11-13 00:15:37 +0000

    Version bump

--- signupto/__init__.py
+++ signupto/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 
 __author__ = 'Luke Plant'
 __email__ = 'L.Plant.98@cantab.net'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from .client import Client, HashAuthorization, TokenAuthorization, ClientError, ObjectNotFound
